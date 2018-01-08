be5d4acc1e60c63af4a9f7b499feac617a19cb29
sandym17 <32313144+sandym17@users.noreply.github.com>
2018-01-08 14:53:58 -0500

    SomeComment

---
+++ GitSample/GitSample/PageObjects/SearchPage.cs
@@ -0,0 +1,38 @@
+﻿using GitSample;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitSample
+{
+    class SearchPage
+    {
+        public SearchPage()
+        {
+            PageFactory.InitElements(Properties.driver, this);
+        }
+
+        [FindsBy(How = How.Name, Using = "btnK")]
+        public IWebElement searchButton { get; set; }
+
+        [FindsBy(How = How.Id, Using = "lst-ib")]
+        public IWebElement searchBox { get; set; }
+
+       
+
+        public void GoogleSearch(string searchText)
+        {
+            searchBox.EnterText(searchText);
+            System.Threading.Thread.Sleep(10000);
+            searchButton.Click();
+            System.Threading.Thread.Sleep(5000);
+
+        }
+
+
+    }
+}
---
+++ GitSample/GitSample/SetMethods.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Support.UI;
+
+namespace GitSample
+{
+    public static class PortalSetMethods
+    {
+        //Enter Text
+        public static void EnterText(this IWebElement element, string value)
+        {
+            element.SendKeys(value);
+        }
+
+        //Click
+        public static void Click(this IWebElement element)
+        {
+            element.Click();
+        }
+
+        //Dropdown Control
+        public static void Dropdown(this IWebElement element, string value)
+        {
+            new SelectElement(element).SelectByText(value);
+        }
+    }
+}
