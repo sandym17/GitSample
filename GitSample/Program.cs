a57b3104233596473848ad98a378dabdaaa7a079
sandym17 <32313144+sandym17@users.noreply.github.com>
2018-01-09 10:34:04 -0500

    Add files via upload

---
+++ GitSample/Program.cs
@@ -0,0 +1,46 @@
+﻿using GitSample;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitSampleTest
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+        }
+
+        [SetUp]
+        public void Initialize()
+        {
+            Properties.driver = new ChromeDriver(@"C:\Program Files\Chrome");
+            //Navigate to Google Page test
+            Properties.driver.Navigate().GoToUrl("https://www.google.com/");
+
+        }
+
+
+        [Test, Order(1)]
+        public void SearchTest()
+        {
+            SearchPage page = new SearchPage();
+            page.GoogleSearch("test");
+
+        }
+
+
+
+        [TearDown]
+        public void CleanUp()
+        {
+           Properties.driver.Close();
+         }
+    }
+}
---
+++ GitSample/Properties.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitSample
+{
+    // enum is not used anywhere in the scripts
+    enum PropertyType
+    {
+        Id,
+        Name,
+        LinkText,
+        CssName,
+        XPath,
+
+    }
+
+}
+class Properties
+{
+    public static IWebDriver driver { get; set; }
+
+}
